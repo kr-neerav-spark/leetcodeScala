2fb2b8eab9a95d871d927168d55fc6568b3ab849
Neerav Kumar <kr.neerav.spark@gmail.com>
2020-05-28 21:27:57 -0700

    isPalindrome

--- leetcodeScala/src/main/scala/p9.scala
+++ leetcodeScala/src/main/scala/p9.scala
@@ -2,14 +2,15 @@ package main.scala
 
 class p9 {
   def isPalindrome(x: Int): Boolean = {
-    if (x<0) false else x == recursiveParlindrome(x,0)
+    // if number is negative return false
+    if (x<0) false else x == recursiveReverse(x,0)
   }
-
-  def recursiveParlindrome(inpt: Int, rev: Int): Int = {
+  // recursive function to find the reverse of the number
+  def recursiveReverse(inpt: Int, rev: Int): Int = {
     var r = inpt%10
     inpt match {
-      case 0 => rev
-      case _ => recursiveParlindrome(inpt/10, rev*10 + r)
+      case 0 => rev // if inpt is 0 then return the reverse number
+      case _ => recursiveReverse(inpt/10, rev*10 + r)
     }
   }
 }
